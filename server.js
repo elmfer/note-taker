fb821230eb9b6ce787a022bdc77f415ba3cc456d
elmfer <elmfer11@outlook.com>
2023-10-26 01:34:36 -0500

    added database functionality

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const api = require('./lib/app');
 
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 
 const app = express();
 
 app.use(express.static("public"));
+app.use('/api', api);
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
